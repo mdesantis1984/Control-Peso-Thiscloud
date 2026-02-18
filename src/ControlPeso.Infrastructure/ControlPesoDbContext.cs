d8ae0943651c23e2b25ca5fbb1bbfbfcf13b92d3
Marco Alejandro De Santis <mdesantis@thiscloud.com.ar>
2026-02-18 12:58:49 +0100

    fix(ci): support InMemory provider in tests - check IsConfigured before UseSqlite

--- src/ControlPeso.Infrastructure/ControlPesoDbContext.cs
+++ src/ControlPeso.Infrastructure/ControlPesoDbContext.cs
@@ -25,8 +25,14 @@ public partial class ControlPesoDbContext : DbContext
     public virtual DbSet<WeightLogs> WeightLogs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Solo configurar SQLite si no hay provider ya configurado (permite InMemory en tests)
+        if (!optionsBuilder.IsConfigured)
+        {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=../../controlpeso.db");
+            optionsBuilder.UseSqlite("Data Source=../../controlpeso.db");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
